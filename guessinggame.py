4119fad1b87b5ee9c1f8f6d40c2d7fde5b837aee
8059586 <70647848+8059586@users.noreply.github.com>
2021-06-06 10:47:19 -0400

    Add files via upload

---
+++ guessinggame.py
@@ -0,0 +1,11 @@
+chances = 0
+random = 1,9
+guess = 1
+number = 1,9
+while chances < 5:
+    random
+    if guess == number:
+        print("CONGRATULATIONS! YOU WON!")
+        break
+    if not chances < 5:
+        print("You lose. The number is", number)
